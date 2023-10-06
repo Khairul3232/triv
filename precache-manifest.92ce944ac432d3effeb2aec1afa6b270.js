6f1160aeebffa4a006cd6a1009cb548a6ddc258d
Khai <62913677+Khairul3232@users.noreply.github.com>
2023-10-06 09:07:08 -0600

    Updates

---
+++ precache-manifest.92ce944ac432d3effeb2aec1afa6b270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fed365da123f38ebb93",
+    "url": "/triv/static/css/main.adec9f24.chunk.css"
+  },
+  {
+    "revision": "7fed365da123f38ebb93",
+    "url": "/triv/static/js/main.0c0a0d39.chunk.js"
+  },
+  {
+    "revision": "3812fa315f15dda29cc4",
+    "url": "/triv/static/js/runtime~main.1ad88857.js"
+  },
+  {
+    "revision": "60c7b3b1e3c9a8a81249",
+    "url": "/triv/static/js/2.4723208f.chunk.js"
+  },
+  {
+    "revision": "f019a8d8b6b121e06049",
+    "url": "/triv/static/js/3.ea86ce1b.chunk.js"
+  },
+  {
+    "revision": "b3423b6423cc455e098b2051d45ddd04",
+    "url": "/triv/index.html"
+  }
+];
